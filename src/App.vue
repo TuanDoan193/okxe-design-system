2ea79d9dc73b55c4e552c8bb715a3cd34a7e8d5b
huyennguyen94 <h.huyennguyen@okxe.vn>
2022-08-05 17:18:06 +0700

    create okxe toggle

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <!-- Button -->
+        <h2>Button</h2>
+        <div class="default-style">
+          <span>Button default</span>
+          <okxe-button label="Default" @click="clickFunc">
+            <template #icon>
+              <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+          </okxe-button>
+        </div>
+        <div class="default-style">
+          <span>Large Button</span>
+          <okxe-button label="Large" backgroundColor="gray" large width="300">
+            <template #icon>
+              <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+          </okxe-button>
+        </div>
+        <div class="default-style">
+          <span>Medium button</span>
+          <okxe-button label="Meium" backgroundColor="white" medium>
+            <template #icon>
+              <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+          </okxe-button>
+        </div>
+        <div class="default-style">
+          <span>Small Button</span>
+          <okxe-button label="Small" backgroundColor="red" small width="107" borderRadius="16">
+          </okxe-button>
+        </div>
+        <div class="default-style">
+          <span>Disable Button</span>
+          <okxe-button
+              colorText="black"
+              disabled
+              border-radius="8"
+              width="200px"
+              medium
+              label="disable button"
+          />
+        </div>
+
+        <!-- circle -->
+         <okxe-button
+            background-color="white"
+            circleButton="large"
+        >
+            <template #icon>
+               <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+        </okxe-button>
+        <okxe-button
+            background-color="white"
+            circleButton="medium"
+        >
+            <template #icon>
+               <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+        </okxe-button>
+
+        <okxe-button
+            background-color="rgba(18, 22, 23, 0.5)"
+            circleButton="small"
+        >
+            <template #icon>
+               <v-img src="/images/badge_facebook.png" width="16" height="16"></v-img>
+            </template>
+        </okxe-button>
+        <v-divider></v-divider>
+        <!-- okxe toggle -->
+        <h2>Okxe Toggle</h2>
+        <okxe-toggle
+          v-model="checkbox"
+        >
+          <span>
+            Test checkbox
+          </span>
+        </okxe-toggle>
+        <!-- end okxe toggle -->
+        <okxe-button
+            bottom-fixed
+            border-radiu="0"
+            backgroundColor="#00BCC3"
+            large
+            label="Xác Nhận (Bottom button)"
+        >
+        </okxe-button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import OkxeButton from './components/OkxeButton.vue'
+import OkxeToggle from './components/OkxeToggle.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    OkxeButton,
+    OkxeToggle
+  },
+  data() {
+    return {
+      checkbox: false,
+    }
+  },
+  methods: {
+    clickFunc() {
+      alert("click in default button")
+    },
   }
 }
 </script>
@@ -25,4 +125,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+<style lang="sass" scoped>
+.default-style
+    display: flex
+    justify-content: center
+    align-items: baseline
+
 </style>
